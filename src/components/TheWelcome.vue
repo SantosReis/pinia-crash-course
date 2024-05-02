c142370177bd9dea255a5851da91f0b326350ff9
Marcus Reis <mvsantosreis@gmail.com>
2024-05-02 12:22:45 +0100

    Get State from Pinia (legacy)

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,8 +1,11 @@
 <script setup>
 import { useCounterStore } from "@/stores/counter";
+import Counter from "@/components/Counter.vue";
 const store = useCounterStore();
 console.log(store.count);
 </script>
 <template>
+  <p>{{ store.count }}</p>
   <h2>Welcome</h2>
+  <Counter :count="store.count" />
 </template>
